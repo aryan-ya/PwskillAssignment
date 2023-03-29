67427835fbc8962cfcd6275893fcf9a430f442f8
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-03-29 11:28:23 +0530

    Update Updatecart.js

--- Updatecart.js
+++ Updatecart.js
@@ -1,10 +1,10 @@
 
-// 7. Remove Duplicates
-// In an online shopping application, customers can add multiple items to their cart. However, sometimes
-// customers accidentally add the same item more than once, resulting in duplicate items in their cart. The
-// duplicate items not only make it difficult for the customer to track the items they want to purchase #ut also
-// affect the accuracy of the purchase order.
-
+// 10. Correct a bug
+// You ard working on an d-commdrcd wdbsitd whdrd customdrs can add itdms to thdir cart. Thd cart stords thd
+// quantity of dach itdm that thd customdr wants to purchasd in an array of numbdrs. Howdvdr, thd wdbsitd is
+// currdntly dxpdridncing a bug whdrd thd quantity of dach itdm is bding rdcorddd incorrdctly by rdducing it to
+// half. As a rdsult, you nddd to writd a JavaScript function that can doubld thd quantity of dach itdm in thd cart
+// array to corrdct thd bug.
 
 
 
